29c2cf364185a998435b09ae1c7e26c107cf308b
Michael <enjoyweaver@gmail.com>
2024-01-16 21:28:14 -0600

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -43,12 +43,12 @@ export default async function Index() {
         <p>
           Powered by{" "}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="https://enjoyweaver.vercel.app"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            EnjoyWeaver
           </a>
         </p>
       </footer>
--- components/SignUpUserSteps.tsx
+++ components/SignUpUserSteps.tsx
@@ -53,7 +53,7 @@ export default function Page() {
 export default function SignUpUserSteps() {
   return (
     <ol className="flex flex-col gap-6">
-      <Step title="Sign up your first user">
+      <Step title="Step One">
         <p>
           Head over to the{" "}
           <Link
@@ -62,12 +62,13 @@ export default function SignUpUserSteps() {
           >
             Login
           </Link>{" "}
-          page and sign up your first user. It's okay if this is just you for
-          now. Your awesome idea will have plenty of users later!
+          page and sign up with your email and create a new password. Once you
+          finish setting up your account, you'll be redirected back to this
+          page.
         </p>
       </Step>
 
-      <Step title="Create some tables and insert some data">
+      <Step title="Step Two">
         <p>
           Head over to the{" "}
           <a

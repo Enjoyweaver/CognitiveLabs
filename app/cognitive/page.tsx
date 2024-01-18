066498f34d6a8d7caeaa623155f09e4ad78e4cb9
Michael <enjoyweaver@gmail.com>
2024-01-17 22:01:46 -0600

    mobile styling

--- app/cognitive/page.tsx
+++ app/cognitive/page.tsx
@@ -1,13 +1,13 @@
 // Cognitive.tsx
 import Link from "next/link";
-import { headers, cookies } from "next/headers";
-import Header from "@/components/Header";
-import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import AuthButton from "@/components/AuthButton"; // Import AuthButton if needed
+import { createClient } from "@/utils/supabase/server";
+import Header from "@/components/Header";
 
 const signOut = async () => {
   "use server";
-
   const cookieStore = cookies();
   return redirect("/login");
 };
@@ -15,33 +15,36 @@ const signOut = async () => {
 export default function Cognitive() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="flex w-full justify-between items-center p-3 border-b border-foreground/10">
-        <Link
-          href="/"
-          className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <Link
+            href="/"
+            className="py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
           >
-            <polyline points="15 18 9 12 15 6" />
-          </svg>{" "}
-          Back
-        </Link>
-        <form action={signOut}>
-          <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-            Logout
-          </button>
-        </form>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+            >
+              <polyline points="15 18 9 12 15 6" />
+            </svg>{" "}
+            Back
+          </Link>
+          <form action={signOut}>
+            <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+              Logout
+            </button>
+          </form>
+        </div>
+      </nav>
+
       <main className="pt-10 flex-1 flex flex-col gap-6 max-w-4xl">
         <h2 className="text-center font-bold text-4xl mb-4">
           Cognitive Planning

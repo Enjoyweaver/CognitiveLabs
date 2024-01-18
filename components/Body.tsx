b9dd51eb490d078c1fc5bfec063111965770aaf2
Michael <enjoyweaver@gmail.com>
2024-01-17 21:03:29 -0600

    styling

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <p className="text-3xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center">
+      <p className="text-2xl !leading-tight mx-auto max-w-xl text-center">
         Its simple, you input your tasks and the timeframe you need them done,
         we analyze them in our Cognitive Calculator and then propose a schedule
         that will maximize efficiency and productivity for your timeframe.

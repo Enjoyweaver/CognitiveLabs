65aec0cdb2589f769a7b90519c80c748ef4ee257
Michael <enjoyweaver@gmail.com>
2024-01-19 15:44:12 -0600

    mobile styling

--- app/cognitive/page.tsx
+++ app/cognitive/page.tsx
@@ -15,7 +15,7 @@ const signOut = async () => {
 
 export default function Cognitive() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center px-3">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <Link

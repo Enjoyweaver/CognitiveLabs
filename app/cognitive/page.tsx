4bf1183de6e0d905b1d4b0bd5d3d12732d0d870c
Michael <enjoyweaver@gmail.com>
2024-01-19 16:36:46 -0600

    styling

--- app/cognitive/page.tsx
+++ app/cognitive/page.tsx
@@ -15,7 +15,7 @@ const signOut = async () => {
 
 export default function Cognitive() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center px-3">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center justify-center px-3">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <Link
@@ -46,7 +46,7 @@ export default function Cognitive() {
         </div>
       </nav>
 
-      <main className="flex-1 w-full flex flex-col gap-20 items-center">
+      <main className="flex-1 w-full flex flex-col gap-20 justify-center items-center">
         <h2 className="text-center font-bold text-4xl mb-4">
           Cognitive Planning
         </h2>
@@ -61,113 +61,108 @@ export default function Cognitive() {
           are all of the cognitive functions, but just that these are the ones
           we'll be working with in this app:
         </p>
-        <main className="flex-1 flex flex-col gap-20 items-center">
-          <div className="flex justify-content: space-evenly">
-            {/* Column 1: Cognitive Functions */}
-            <div
-              className={`bg-blue-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
-            >
-              <h3 className="text-white text-lg font-bold mb-2 h-20">
-                Cognitive Functions
-              </h3>
-              <ul className="list-disc pl-3 text-white text-lg">
-                <li className={styles.row}>Analytical</li>
-                <li className={styles.row}>Perceptual</li>
-                <li className={styles.row}>Creative</li>
-                <li className={styles.row}>Conceptual</li>
-                <li className={styles.row}>Strategic</li>
-                <li className={styles.row}>Administrative</li>
-                <li className={styles.row}>Technical</li>
-                <li className={styles.row}>Collaborative</li>
-              </ul>
-            </div>
-
-            <div
-              className={`bg-green-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
-            >
-              <h3 className="text-white text-lg font-bold mb-2 h-20">
-                Definitions
-              </h3>
-              <ul className="list-disc pl-3 text-white text-lg">
-                <li className={styles.row}>
-                  More than one piece may fit into the puzzle, but not all bring
-                  the same value.
-                </li>
-                <li className={styles.row}>
-                  Viewing the puzzles from different angles instead of from
-                  above the coffee table.
-                </li>
-                <li className={styles.row}>
-                  Generating ideas and solutions in an unstructured and
-                  free-flowing manner.
-                </li>
-                <li className={styles.row}>
-                  Creating concepts and forming innovative connections between
-                  ideas.
-                </li>
-                <li className={styles.row}>
-                  Using your resources to the best of their ability, even if
-                  they aren't designed for that purpose.
-                </li>
-                <li className={styles.row}>
-                  Organizing and managing tasks, resources, and people
-                  efficiently.
-                </li>
-                <li className={styles.row}>
-                  Applying specialized knowledge and skills to solve practical
-                  problems.
-                </li>
-                <li className={styles.row}>
-                  Working cooperatively with others to achieve shared
-                  objectives.
-                </li>
-              </ul>
-            </div>
-
-            {/* Column 3: Examples */}
-            <div
-              className={`bg-orange-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
-            >
-              <h3 className="text-white text-lg font-bold mb-2 h-20">
-                Examples
-              </h3>
-              <ul className="list-disc pl-3 text-white text-lg">
-                <li className={styles.row}>
-                  Some people complete a puzzle best by starting with the edges,
-                  others by the colors, and others just start.
-                </li>
-                <li className={styles.row}>
-                  The kids' new clothing as a chore, but it could be viewed as a
-                  chance to help them define their style.
-                </li>
-                <li className={styles.row}>
-                  Approaching a problem by generating as many ideas as possible
-                  without worrying about structure or feasibility.
-                </li>
-                <li className={styles.row}>
-                  Making connections between seemingly unrelated concepts to
-                  create innovative solutions.
-                </li>
-                <li className={styles.row}>
-                  Developing a step-by-step plan with milestones and deadlines
-                  to achieve a long-term objective.
-                </li>
-                <li className={styles.row}>
-                  Efficiently organizing tasks, resources, and team members to
-                  achieve project goals.
-                </li>
-                <li className={styles.row}>
-                  Applying technical expertise to solve practical problems and
-                  challenges.
-                </li>
-                <li className={styles.row}>
-                  Collaborating with team members, leveraging each other's
-                  strengths, to achieve common goals.
-                </li>
-              </ul>
-            </div>
+        <div className="flex justify-content: space-evenly">
+          {/* Column 1: Cognitive Functions */}
+          <div
+            className={`bg-blue-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
+          >
+            <h3 className="text-white text-lg font-bold mb-2 h-20">
+              Cognitive Functions
+            </h3>
+            <ul className="list-disc pl-3 text-white text-lg">
+              <li className={styles.row}>Analytical</li>
+              <li className={styles.row}>Perceptual</li>
+              <li className={styles.row}>Creative</li>
+              <li className={styles.row}>Conceptual</li>
+              <li className={styles.row}>Strategic</li>
+              <li className={styles.row}>Administrative</li>
+              <li className={styles.row}>Technical</li>
+              <li className={styles.row}>Collaborative</li>
+            </ul>
           </div>
-        </main>
+
+          <div
+            className={`bg-green-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
+          >
+            <h3 className="text-white text-lg font-bold mb-2 h-20">
+              Definitions
+            </h3>
+            <ul className="list-disc pl-3 text-white text-lg">
+              <li className={styles.row}>
+                More than one piece may fit into the puzzle, but not all bring
+                the same value.
+              </li>
+              <li className={styles.row}>
+                Viewing the puzzles from different angles instead of from above
+                the coffee table.
+              </li>
+              <li className={styles.row}>
+                Generating ideas and solutions in an unstructured and
+                free-flowing manner.
+              </li>
+              <li className={styles.row}>
+                Creating concepts and forming innovative connections between
+                ideas.
+              </li>
+              <li className={styles.row}>
+                Using your resources to the best of their ability, even if they
+                aren't designed for that purpose.
+              </li>
+              <li className={styles.row}>
+                Organizing and managing tasks, resources, and people
+                efficiently.
+              </li>
+              <li className={styles.row}>
+                Applying specialized knowledge and skills to solve practical
+                problems.
+              </li>
+              <li className={styles.row}>
+                Working cooperatively with others to achieve shared objectives.
+              </li>
+            </ul>
+          </div>
+
+          {/* Column 3: Examples */}
+          <div
+            className={`bg-orange-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
+          >
+            <h3 className="text-white text-lg font-bold mb-2 h-20">Examples</h3>
+            <ul className="list-disc pl-3 text-white text-lg">
+              <li className={styles.row}>
+                Some people complete a puzzle best by starting with the edges,
+                others by the colors, and others just start.
+              </li>
+              <li className={styles.row}>
+                The kids' new clothing as a chore, but it could be viewed as a
+                chance to help them define their style.
+              </li>
+              <li className={styles.row}>
+                Approaching a problem by generating as many ideas as possible
+                without worrying about structure or feasibility.
+              </li>
+              <li className={styles.row}>
+                Making connections between seemingly unrelated concepts to
+                create innovative solutions.
+              </li>
+              <li className={styles.row}>
+                Developing a step-by-step plan with milestones and deadlines to
+                achieve a long-term objective.
+              </li>
+              <li className={styles.row}>
+                Efficiently organizing tasks, resources, and team members to
+                achieve project goals.
+              </li>
+              <li className={styles.row}>
+                Applying technical expertise to solve practical problems and
+                challenges.
+              </li>
+              <li className={styles.row}>
+                Collaborating with team members, leveraging each other's
+                strengths, to achieve common goals.
+              </li>
+            </ul>
+          </div>
+        </div>
       </main>
 
       <div className="mb-4 max-w-lg mx-auto">

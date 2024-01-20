205b0dc7e06dad55858c6813f2d0f4c924b7d651
Michael <enjoyweaver@gmail.com>
2024-01-20 12:19:46 -0600

    "...abilities"

--- app/cognitive/page.tsx
+++ app/cognitive/page.tsx
@@ -6,6 +6,7 @@ import AuthButton from "@/components/AuthButton"; // Import AuthButton if needed
 import { createClient } from "@/utils/supabase/server";
 import Header from "@/components/Header";
 import styles from "./CognitiveStyles.module.css";
+import Sources from "@/components/Sources";
 
 const signOut = async () => {
   "use server";
@@ -56,7 +57,7 @@ export default function Cognitive() {
           efficiency and productivity.
         </p>
         <p className="text-1.5xl !leading-tight mx-auto max-w-xl text-center">
-          Here are the cognitive functions we'll be working with and more
+          Here are the cognitive abilities we'll be working with and more
           information about them, though do know that we are not saying these
           are all of the cognitive functions, but just that these are the ones
           we'll be working with in this app:
@@ -67,7 +68,7 @@ export default function Cognitive() {
             className={`bg-blue-500 rounded-tl-md rounded-bl-md p-4 flex flex-col items-center`}
           >
             <h3 className="text-white text-lg font-bold mb-2 h-20">
-              Cognitive Functions
+              Cognitive Abilities
             </h3>
             <ul className="list-disc pl-3 text-white text-lg">
               <li className={styles.row}>Analytical</li>
@@ -164,76 +165,8 @@ export default function Cognitive() {
           </div>
         </div>
       </main>
+      <Sources />
 
-      <div className="mb-4 max-w-lg mx-auto">
-        <h3 className="font-bold text-sm mb-2 text-center">Sources:</h3>
-        <ul className="list-disc pl-4 ">
-          <li>
-            <a
-              href="https://www.tandfonline.com/doi/abs/10.1080/13546783.2011.625663"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Time of day effects on problem solving: When the non-optimal is
-              optimal
-            </a>{" "}
-            by Mareike B. Wieth, Rose T. Zacks (Published: Dec, 2011)
-          </li>
-          <li>
-            <a
-              href="https://www.apa.org/monitor/2018/06/good-timing"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Psychologists research highlights how timing can affect memory,
-              attention, mood and more
-            </a>{" "}
-            by Kirsten Weir (Published: June, 2018)
-          </li>
-          <li>
-            <a
-              href="https://www.frontiersin.org/articles/10.3389/fnhum.2017.00188/full"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Identifying the Best Times for Cognitive Functioning
-            </a>{" "}
-            by M. D. R. Evans, Paul Kelley, Jonathan Kelley
-          </li>
-          <li>
-            <a
-              href="https://answers.childrenshospital.org/circadian-rhythms-synapses/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Timing is everything: How circadian rhythms influence our brains
-            </a>{" "}
-            by Nancy Fliesler
-          </li>
-          <li>
-            <a
-              href="https://apps.dtic.mil/sti/tr/pdf/ADA064445.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Cognitive Processes in Planning
-            </a>{" "}
-            by Barbara Hayes-Roth, Frederick Hayes-Roth (December 1978)
-          </li>
-          <li>
-            <a
-              href="https://www.cell.com/cell/fulltext/S0092-8674(15)00375-X"
-              target="_blank"
-              rel="noreferrer"
-            >
-              The Circadian Protein BMAL1 Regulates Translation in Response to
-              S6K1-Mediated Phosphorylation
-            </a>{" "}
-            by Jonathan O. Lipton, Elizabeth D. Yuan, Lara M. Boyle, Fred Davis,
-            John M. Asara, Mustafa Sahin (Published: May 14, 2015)
-          </li>
-        </ul>
-      </div>
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Created by{" "}
---
+++ components/Sources.tsx
@@ -0,0 +1,76 @@
+export default function Sources() {
+  return (
+    <div className="flex flex-col gap-10 items-center">
+      <div className="mb-4 max-w-lg mx-auto">
+        <h3 className="font-bold text-sm mb-2 text-center">Sources:</h3>
+        <ul className="list-disc pl-4 ">
+          <li>
+            <a
+              href="https://www.tandfonline.com/doi/abs/10.1080/13546783.2011.625663"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Time of day effects on problem solving: When the non-optimal is
+              optimal
+            </a>{" "}
+            by Mareike B. Wieth, Rose T. Zacks (Published: Dec, 2011)
+          </li>
+          <li>
+            <a
+              href="https://www.apa.org/monitor/2018/06/good-timing"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Psychologists research highlights how timing can affect memory,
+              attention, mood and more
+            </a>{" "}
+            by Kirsten Weir (Published: June, 2018)
+          </li>
+          <li>
+            <a
+              href="https://www.frontiersin.org/articles/10.3389/fnhum.2017.00188/full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Identifying the Best Times for Cognitive Functioning
+            </a>{" "}
+            by M. D. R. Evans, Paul Kelley, Jonathan Kelley
+          </li>
+          <li>
+            <a
+              href="https://answers.childrenshospital.org/circadian-rhythms-synapses/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Timing is everything: How circadian rhythms influence our brains
+            </a>{" "}
+            by Nancy Fliesler
+          </li>
+          <li>
+            <a
+              href="https://apps.dtic.mil/sti/tr/pdf/ADA064445.pdf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Cognitive Processes in Planning
+            </a>{" "}
+            by Barbara Hayes-Roth, Frederick Hayes-Roth (December 1978)
+          </li>
+          <li>
+            <a
+              href="https://www.cell.com/cell/fulltext/S0092-8674(15)00375-X"
+              target="_blank"
+              rel="noreferrer"
+            >
+              The Circadian Protein BMAL1 Regulates Translation in Response to
+              S6K1-Mediated Phosphorylation
+            </a>{" "}
+            by Jonathan O. Lipton, Elizabeth D. Yuan, Lara M. Boyle, Fred Davis,
+            John M. Asara, Mustafa Sahin (Published: May 14, 2015)
+          </li>
+        </ul>
+      </div>
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    </div>
+  );
+}

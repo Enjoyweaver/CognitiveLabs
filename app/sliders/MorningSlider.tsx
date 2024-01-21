64aa78875c5f4593cb0c923cdd9617f3c9b47c40
Michael <enjoyweaver@gmail.com>
2024-01-21 14:55:38 -0600

    functionform

--- app/sliders/MorningSlider.tsx
+++ app/sliders/MorningSlider.tsx
@@ -2,25 +2,17 @@
 import React, { useState, ChangeEvent, useEffect } from "react";
 import "./MorningSlider.css";
 
-const MorningSlider: React.FC = () => {
-  const [value, setValue] = useState<number>(50);
+interface MorningSliderProps {
+  value: number;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const sliderValue = parseInt(event.target.value, 10);
-    setValue(sliderValue);
-    document.documentElement.style.setProperty(
-      "--value",
-      sliderValue.toString()
-    );
-  };
-
+const MorningSlider: React.FC<MorningSliderProps> = ({ value, onChange }) => {
+  const shouldShift = value > -1 && value < 101;
   useEffect(() => {
     // Set initial value to 50% when the component mounts
     document.documentElement.style.setProperty("--value", "50");
   }, []);
-
-  const shouldShift = value > -1 && value < 101;
-
   return (
     <div className="morning-slider-container">
       <div className="control">
@@ -30,7 +22,7 @@ const MorningSlider: React.FC = () => {
           min="0"
           max="100"
           value={value}
-          onChange={handleChange}
+          onChange={onChange}
           className="slider-input"
         />
         <div
